1580ebae5513f235d9bfeab439c823356ba779ef
icewizzard <yoniroit12310@gmail.com>
2025-08-28 14:07:55 +0300

    bg image not routing between pages

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./components/AuthLayout";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";  // ✅ import
+import "./styles/global.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} /> {/* ✅ new route */}
+        </Route>
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ my-react-app/src/components/AuthLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import "../Pages/login.css"; // reuse login.css for layout styles too
+
+export default function AuthLayout() {
+  return (
+    
+    <div className="auth-layout">
+      <Outlet />
+    </div>
+  );
+}
+
