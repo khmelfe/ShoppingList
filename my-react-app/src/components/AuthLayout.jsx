c170302b17cfdc04e7a4347a029404b95576f81c
khmelfe@ac.sce.ac.il <khmelfe@ac.sce.ac.il>
2025-08-28 15:51:08 +0300

    Fixed_Background issue

--- my-react-app/src/components/AuthLayout.jsx
+++ my-react-app/src/components/AuthLayout.jsx
@@ -3,10 +3,10 @@ import "../Pages/login.css"; // reuse login.css for layout styles too
 
 export default function AuthLayout() {
   return (
-    
     <div className="auth-layout">
       <Outlet />
     </div>
+    
   );
 }
 
